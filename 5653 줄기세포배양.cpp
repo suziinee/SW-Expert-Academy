5d099b0eedbae16f4e60a928c5318caa7e6f9ea4
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-10-12 21:51:08 +0900

    Soltuion 2 - 우선순위 큐 이용 : turn 오름차순, life 내림차순 - cell 구조체 : y, x 좌표 + 생명력 life + 활성화 될때까지 기다려야 하는 시간 wait + 현재 턴 turn + 활성화 되었는지 여부 active - 초기에는 turn을 0으로 시작해서 while문에서 turn이 K가 되면 pq.size() 리턴하면서 종료 - active 상태인 세포     - 1. 사방으로 번식 : 이때 이미 방문되어 있으면 번식할 수 없음(pq에서 이미 정렬했기 때문에 따로 확인하지 않음)     - 2. 생명력이 줄어듦 -> 0이 아니면 다시 pq에 push - inactive 상태인 세포     - wait이 줄어들고 0이 되면 active를 true로 해서 pq에 push - turn은 매번 +1씩 됨 -> bfs

--- 5653 줄기세포배양.cpp
+++ 5653 줄기세포배양.cpp
@@ -1,77 +1,60 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
-#include <unordered_map>
-#include <unordered_set>
 using namespace std;
 
+#define MAX 400
 int N, M, K;
+bool chk[MAX + 1][MAX + 1];
 
 struct CELL {
-	int y, x, life, time;
-	bool operator<(const CELL& c) const {
-		return time > c.time;
+	int y, x;
+	int life, wait, turn;
+	bool active;
+	bool operator<(const CELL& c) const { //turn 오름차순, life 내림차순
+		if (turn == c.turn) return life < c.life;
+		return turn > c.turn;
 	}
 };
 priority_queue<CELL> pq;
-priority_queue<int, vector<int>, greater<int>> pq_after; //번식 이후의 세포들 관리(죽는 시간 넣음)
-
-inline long long int get_key(int a, int b) {
-	return (long long)a * 100000 + (long long)b;
-}
-unordered_set<long long int> ht; //해당 좌표 사용되었는지 여부
 
 
-int solve(int t)
+int solve()
 {
 	static int dx[] = { 0, 1, 0, -1 };
 	static int dy[] = { -1, 0, 1, 0 };
-	int die = 0;
+	
+	while (!pq.empty()) {
+		CELL cur = pq.top(); pq.pop();
 
-	while (true) {
-		//현재 time에서 새로 만들어지는 세포들 좌표
-		unordered_map<long long int, CELL> born;
+		if (cur.turn == K) {
+			pq.push(cur);
+			return pq.size();
+		}
 
-		//현재 활성상태 세포 꺼내서 번식하기
-		while (!pq.empty() && pq.top().time == t) {
-			CELL cur = pq.top(); pq.pop();
-			pq_after.push(t + cur.life);
-
+		//사방으로 번식 + cur의 생명력 줄어듦
+		if (cur.active) {
 			for (int d = 0; d < 4; d++) {
 				int nx = cur.x + dx[d];
 				int ny = cur.y + dy[d];
-				int key = get_key(ny, nx);
-				if (ht.find(key) != ht.end()) continue; //다른 세포가 이미 번식한 자리
-				if (born.find(key) == born.end()) {
-					born.insert({ key, {ny, nx, cur.life, cur.life + t + 1} });
-				}
-				else {
-					CELL& tmp = born[key];
-					if (tmp.life < cur.life) {
-						born[key] = { ny, nx, cur.life, cur.life + t + 1 }; //교체
-					}
-				}
+				if (chk[ny][nx]) continue;
+				pq.push({ ny, nx, cur.life, cur.life, cur.turn + 1, false });
+				chk[ny][nx] = true;
+			}
+			if (--cur.life > 0) { //생명력이 0이 아니면 사망한게 아니므로 다시 pq에 넣어줌
+				cur.turn++;
+				pq.push(cur);
 			}
 		}
-
-		//번식 끝난 비활성 상태 중 현재 t에 죽은 세포들 꺼내기
-		while (!pq_after.empty() && pq_after.top() == t) {
-			pq_after.pop();
-			die++;
-		}
-
-		++t;
-		if (t > K) break;
-
-		//새로 생성된 세포를 ht에 추가하기
-		if (!born.empty()) {
-			for (auto it = born.begin(); it != born.end(); it++) {
-				ht.insert(it->first);
-				pq.push(it->second);
+		//대기시간 줄어듦
+		else {
+			if (--cur.wait == 0) {
+				cur.active = true;
 			}
+			cur.turn++;
+			pq.push(cur);
 		}
 	}
-
-	return pq.size() + pq_after.size();
 }
 
 void input()
@@ -80,24 +63,19 @@ void input()
 	cin >> T;
 	for (int t = 1; t <= T; t++) {
 		cin >> N >> M >> K;
-		ht.clear();
+		fill(&chk[0][0], &chk[MAX - 1][MAX], false);
 		pq = {};
-		pq_after = {};
-
 		int n;
-		int min_time = 11;
 		for (int y = 0; y < N; y++) {
 			for (int x = 0; x < M; x++) {
 				cin >> n;
 				if (n) {
-					pq.push({ y, x, n, n });
-					ht.insert(get_key(y, x));
-					if (n < min_time) min_time = n;
+					chk[y + MAX / 2][x + MAX / 2] = true;
+					pq.push({ y + MAX / 2, x + MAX / 2, n, n, 0, false });
 				}
 			}
 		}
-
-		cout << "#" << t << " " << solve(min_time) << "\n";
+		cout << "#" << t << " " << solve() << "\n";
 	}
 }
 
